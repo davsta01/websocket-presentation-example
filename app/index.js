0dafc52ff5c71d02e736bba99cfd1bc07b5a4d85
davsta01 <64217283+davsta01@users.noreply.github.com>
2022-06-11 17:39:57 +0200

    changed ws-address to "localhost" (instead of last IP)

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
     const ws = await connectToServer();  
 
     async function connectToServer() {    
-        const ws = new WebSocket('ws://141.45.58.1:7071/ws');
+        const ws = new WebSocket('ws://localhost:7071/ws');
         return new Promise((resolve, reject) => {
             const timer = setInterval(() => {
                 if(ws.readyState === 1) {
